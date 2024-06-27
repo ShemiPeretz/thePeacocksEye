26cd651cea617db0b2c5996c56cf053a8875090b
ShemiPeretz <shemi.p17@gmail.com>
2024-06-27 16:07:26 +0300

    Added alerts endpoint and parsing

---
+++ src/api/routers/alerts.py
@@ -0,0 +1,67 @@
+import xml.etree.ElementTree as ET
+import json
+import re
+import requests
+from dotenv import load_dotenv
+from fastapi import HTTPException, APIRouter
+
+
+load_dotenv()
+router = APIRouter()
+
+xml_url = 'https://ims.gov.il/sites/default/files/ims_data/rss/alert/rssAlert_general_country_en.xml'
+
+def strip_html_tags(text):
+    # Remove HTML tags using a regular expression
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+def remove_update_prefix(text):
+    # Remove the "update:" prefix if it exists
+    return text.replace('update:', '').strip()
+
+
+def clean_title(text):
+    # Remove the "Updated on:..." part from the title using regex
+    return re.sub(r' Updated on:.*', '', text)
+
+
+def parse_xml_to_json(xml_url):
+    # Fetch the XML content from the URL
+    response = requests.get(xml_url)
+    response.raise_for_status()  # Raise an error if the request was unsuccessful
+
+    # Parse the XML content
+    root = ET.fromstring(response.content)
+
+    items = []
+    for item in root.findall('./channel/item'):
+        title = item.find('title').text
+        title_clean = clean_title(title)
+        description = item.find('description').text
+        date = item.find('pubDate').text
+
+        # Clean the description text
+        description_clean = strip_html_tags(description)
+        description_clean = remove_update_prefix(description_clean)
+
+        item_dict = {
+            'title': title_clean,
+            'description': description_clean,
+            'date': date
+        }
+        items.append(item_dict)
+
+    if not items:
+        return json.dumps("no alerts", indent=4)
+
+    return json.dumps(items, indent=4)
+
+
+@router.get("/get-alerts/")
+def get_alerts():
+    return parse_xml_to_json(xml_url)
+
+# json_result = parse_xml_to_json(xml_url)
+# print(json_result)
